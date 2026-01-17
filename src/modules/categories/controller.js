6efa093ecefb89084563536ac23aee5f717ff53d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:22:27 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/modules/categories/controller.js
+++ src/modules/categories/controller.js
@@ -4,6 +4,7 @@
 // routing muammosi hal qilindi
 // bundle size optimallashtirildi
 // package.json yangilandi
+// middleware funksiyalari qo'shildi
 const { redFile, writeFile } = require("../../utils/fs")
 // API endpoint testlari qo'shildi
 // README faylini yangilash
