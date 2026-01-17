e570e3d3069f6eef6c127e257e5ebc77de5c28a2
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:22:09 +0500

    style: kod uslubini yaxshilash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fileupload  = require("express-fileupload");
+// integration testlar yaratildi
 const router = require("./routes/index")
 const app = express()
 
